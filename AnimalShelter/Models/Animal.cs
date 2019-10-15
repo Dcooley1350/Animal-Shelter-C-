550f8cef84b7bfbd621cfa5c37b4e95a52c4b4b0
Devin Cooley and Jacqueline Remmel <jacqueline@>
2019-10-15 10:32:37 -0700

    Add animal shelter program.

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,13 @@
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Gender { get; set; }
+    public string Breed { get; set; }
+    public string Date_of_admit { get; set; }
+    public int AnimalId { get; set; }
+    public string Description { get; set; }
+  }
+}
